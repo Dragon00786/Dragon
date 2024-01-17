84157c28b0f4f7e7cb38fc30030722cca410bc88
Dragon00786 <156892371+Dragon00786@users.noreply.github.com>
2024-01-17 13:14:46 +0500

    Update DRG.py

--- DRG.py
+++ DRG.py
@@ -1,1 +1,7 @@
 
+import os,platform
+os.system('git pull')
+# exit(' Wait Tool On updating ')
+DRAGON=platform.architecture()[0]
+if DRAGON=="32bit":__import__("DRAGON32")
+elif DRAGON=="64bit":__import__("DRAGON64")
